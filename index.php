3ea8e3b79bf4ea55c6220ae9ebe6ae212a7e9a91
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2015-04-23 11:12:12 -0700

    Milestones 1&2 completed

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Todo-List 2.0</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css"> <!-- this is a link -->
+</head>
+<body>
+<div class="wrap">
+	
+</div>
+
+</body>
+</html>
