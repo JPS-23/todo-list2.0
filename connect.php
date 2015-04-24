dde5f595c2eb4783d93c339a00e8fa3e9d10af6c
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2015-04-24 10:44:14 -0700

    Milestones 5 & 5.1 completed

--- connect.php
+++ connect.php
@@ -1,14 +1,14 @@
 <?php
 //Why is this important? Why does localhost have to be first?
-$mysqli = mysqli('localhost', 'root', 'root', 'tasks');
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo-list2.0');
 //$mysqli->connect_error then we want it to die and have this message
 if ($mysqli->connect_error) {
-	die('Connect Error (' . $mysqli->connect_errno .')')
+	die('Connect Error (' . $mysqli->connect_errno .')'
 		. $mysqli->connect_error);
 }
 else{
 	echo"Connection made";
 }
-$mysqli->close
+$mysqli->close();
 
 ?> 
