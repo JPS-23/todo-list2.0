85ea688c0b65a8925aa420fc47e755f629afc147
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2015-04-24 11:06:24 -0700

    Milestone 7 finished

--- index.php
+++ index.php
@@ -12,11 +12,8 @@
 		</ul>
 	</div>
 	<form class="add-new-task" autocomplete="off">
-	<input type="text" name="new-task" placeholder="Add new item...">
-
+		<input type="text" name="new-task" placeholder="Add new item...">
 	</form>
-
 </div>
-
 </body>
 </html>
